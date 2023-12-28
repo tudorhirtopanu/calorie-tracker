9c04a7d3153d7d069e701295be89f8c36b4f5747
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-28 17:04:14 +0000

    Initial Commit

---
+++ Health App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 28/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health App/Health_App.swift
@@ -0,0 +1,17 @@
+//
+//  Health_AppApp.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Health_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
