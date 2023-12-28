ac28661b253f2d840d4223018ffbab95ca55ec0e
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-28 22:39:05 +0000

    Create data model for food items

---
+++ Health App/Models/FoodItems.swift
@@ -0,0 +1,45 @@
+//
+//  CommonItemType.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 28/12/2023.
+//
+
+import Foundation
+
+struct FoodItems:Codable {
+    let common:[CommonItem]?
+    let branded:[BrandedItem]?
+}
+
+struct CommonItem: Codable {
+    let foodName:String
+    let servingUnit:String
+    let tagName:String
+    let servingQty:Double
+    let commonType:Int?
+    let tagId:String
+    let photo:Photo
+    let locale: String
+}
+
+struct BrandedItem: Codable {
+    let foodName:String
+    let servingUnit:String
+    let nixBrandId:String
+    let brandNameItemName:String
+    let servingQty:Double
+    let nfCalories:Int
+    let photo:Photo
+    let brandName:String
+    let region:Int
+    let brandType:Int
+    let nixItemId:String
+    let locale:String
+}
+
+struct Photo: Codable {
+    let thumb: String
+    let highres: String?
+    let isUserUploaded: Bool?
+}
