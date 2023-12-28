71b593c11b2b100e00f8260bf11c59312ae5d72c
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-28 20:31:24 +0000

    remove original searchInstant function

--- Health App/Models/NutritionixService.swift
+++ Health App/Models/NutritionixService.swift
@@ -14,60 +14,7 @@ class NutritionixService {
     private let appId = Secrets.nutritionixAppId
     private let baseEndpoint = "https://api.nutritionix.com/v2"
     
-    func searchInstant(query:String) {
-        
-        let endpoint = "\(baseEndpoint)/search/instant"
-        
-        guard var components = URLComponents(string: endpoint) else {
-            print("Invalid URL")
-            return
-        }
-        
-        components.queryItems = [
-            URLQueryItem(name: "query", value: query)
-        ]
-        
-        guard let url = components.url else {
-            print("Invalid URL")
-            return
-        }
-        
-        // Create a GET request with the constructed URL
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue(apiKey, forHTTPHeaderField: "x-app-key")
-        request.addValue(appId, forHTTPHeaderField: "x-app-id")
-        
-        URLSession.shared.dataTask(with: request){data ,response, error in
-            
-            // Check for errors in the request
-            if let error = error {
-                print("Error: \(error)")
-                return
-            }
-            
-            // Check if data was received
-            guard let data = data else {
-                print("No data received")
-                return
-            }
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                    print("HTTP Status Code: \(httpResponse.statusCode)")
-            }
-            
-            // Print the raw JSON response to the console
-            if let jsonString = String(data: data, encoding: .utf8) {
-                print("Raw JSON Response: \(jsonString)")
-            } else {
-                print("Error converting data to string")
-            }
-            
-        }.resume()
-        
-    }
-    
-    func searchInstant2(query: String) {
+    func searchInstant(query: String) {
         let url = URL(string: "https://trackapi.nutritionix.com/v2/search/instant/?query=\(query)")!
         
         var request = URLRequest(url: url)
