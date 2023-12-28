6eed375f6492e07c66505515414af95b3fa5ebb6
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-28 20:19:28 +0000

    Retrieve keys

--- Health App/Secrets.swift
+++ Health App/Secrets.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Secrets {
+    
+    static let nutritionixApiKey:String =  {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let config = NSDictionary(contentsOfFile: path),
+              let apiKey = config["NutritionixApiKey"] as? String
+        else {
+            fatalError("Configuration file not found or missing Nutritionix API key.")
+        }
+        return apiKey
+    }()
+    
+    static let nutritionixAppId:String =  {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let config = NSDictionary(contentsOfFile: path),
+              let apiKey = config["NutritionixAppID"] as? String
+        else {
+            fatalError("Configuration file not found or missing Nutritionix APP Id.")
+        }
+        return apiKey
+    }()
+    
+}
