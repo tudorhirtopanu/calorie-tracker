35a7159b5ab339d954724f5513714d26bbdc4c71
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-29 15:11:24 +0000

    add tab view for app tabs

--- Health App/Views/RootView.swift
+++ Health App/Views/RootView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            
+            DailyDiaryView()
+                .tabItem {
+                    Label("Diary", systemImage: "fork.knife")
+                }
+            
+            FoodSearchView()
+                .tabItem { 
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+        }
     }
 }
 
