a8a03a73a47130da6e74dbda92d46e8626269291
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-29 22:37:38 +0000

    add function to fetch data based on id of food

--- Health App/Services/NutritionixService.swift
+++ Health App/Services/NutritionixService.swift
@@ -49,6 +49,42 @@ class NutritionixService {
         }.resume()
     }
     
+    func fetchItemInfo(itemId:String) async {
+        let url = URL(string: "https://trackapi.nutritionix.com/v2/search/item/?nix_item_id=\(itemId)")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        request.addValue(appId, forHTTPHeaderField: "x-app-id")
+        request.addValue(apiKey, forHTTPHeaderField: "x-app-key")
+        
+        // for development set to 0
+        request.addValue("0", forHTTPHeaderField: "x-remote-user-id")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP Status Code: \(httpResponse.statusCode)")
+            }
+            
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print("Raw JSON Response: \(jsonString)")
+            } else {
+                print("Error converting data to string")
+            }
+        }.resume()
+        
+    }
+    
     func searchInstant2(query: String, completion: @escaping (FoodItems?) -> Void) {
         
         let url = URL(string: "https://trackapi.nutritionix.com/v2/search/instant/?query=\(query)")!
