dc0c06a599ddf7ee17d3d155c4c78c39f2147be7
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-30 21:04:16 +0000

    Create reusable button for submitting food items

--- Health App/Views/Reusable Views/SubmitFoodButton.swift
+++ Health App/Views/Reusable Views/SubmitFoodButton.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct SubmitFoodButton: View {
+    
+    @State var mealTitle:String
+    @State var iconWidth:CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader {geo in
+            
+            Button(action: {
+                
+                // Save Data as food item
+                
+            }, label: {
+                VStack{
+                    Image(mealTitle)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: iconWidth, height: 70)
+                    Text(mealTitle)
+                        .font(Font.system(size: 14))
+                    Image(systemName: "plus.circle.fill")
+                        .font(Font.system(size: 22))
+                        .padding([.top, .bottom], 5)
+                }
+            })
+            .foregroundStyle(Color.black)
+            .frame(width: geo.size.width, height: geo.size.height)
+            
+        }
+        
     }
 }
 
 #Preview {
-    SubmitFoodButton()
+    SubmitFoodButton(mealTitle: "Breakfast", iconWidth: 50)
 }
