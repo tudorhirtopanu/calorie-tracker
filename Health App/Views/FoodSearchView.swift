25218ae15a74b142fcc388656ba62c544b27474f
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-30 21:05:29 +0000

    Add navigation link leading to create custom food

--- Health App/Views/FoodSearchView.swift
+++ Health App/Views/FoodSearchView.swift
@@ -13,80 +13,117 @@ struct FoodSearchView: View {
     
     @State private var foodItems: NixFoodItems?
     @State private var isPresented:Bool = false
+    @State private var presentCustomFood:Bool = false
     @State private var selectedFoodItemText: String = ""
-
+    
+    @FocusState private var searchIsFocused:Bool
+    
     
     var body: some View {
         
-        NavigationStack{
-            ZStack {
+        
+        VStack(alignment: .leading) {
+            HStack {
                 
-                VStack(alignment: .leading) {
-                    ZStack {
+                ZStack {
+                    
+                    RoundedRectangle(cornerSize: CGSize( width: 10, height: 10))
+                        .foregroundStyle(Color.gray.opacity(0.15))
+                        .frame(height: 40)
+                    
+                    HStack {
                         
-                        RoundedRectangle(cornerSize: CGSize( width: 10, height: 10))
-                            .foregroundStyle(Color.gray.opacity(0.15))
-                            .frame(height: 40)
+                        Image(systemName: "magnifyingglass")
+                            .foregroundStyle(Color.gray)
+                        TextField("Search for food ...", text: $textField)
+                            .focused($searchIsFocused)
+                    }
+                    .padding(8)
+                }
+                
+                NavigationLink(destination: {
+                    CreateCustomFoodView()
+                }, label: {
+                    VStack {
+                        Image("fork.knife.badge.plus")
+                        Text("Custom")
+                            .font(Font.system(size: 12))
+                    }
+                })
+                
+            }
+            
+            Button(action: {
+                
+                searchIsFocused = false
+                
+                NutritionixService.shared.searchInstant2(query: textField) { result in
+                    self.foodItems = result
+                }
+                
+            }, label: {
+                Text("Search")
+            })
+            .padding([.top, .bottom], 10)
+            
+            Text("Recent Items")
+                .font(.caption)
+                .foregroundStyle(Color.gray)
+            
+            if foodItems == nil {
+                Text("No Recent Items")
+                    .padding(.top, 10)
+            }
+            
+            Spacer()
+            
+            if let foodItems = foodItems {
+                
+                ScrollView {
+                    LazyVGrid(columns:[GridItem(), GridItem()]) {
                         
-                        HStack {
+                        /*
+                         //
+                         //                            ForEach(foodItems.common ?? [], id: \.foodName) { commonItem in
+                         //                                FoodItemRow(foodName: commonItem.foodName, imageURL: commonItem.photo.thumb)
+                         //                                    .padding(.bottom,10)
+                         //                            }
+                         */
+                        
+                        ForEach(foodItems.branded ?? [], id: \.foodName) { brandedItem in
+                            NavigationLink(destination: FoodDetailView(text: brandedItem.foodName, itemId: brandedItem.nixItemId),
+                                           label: {
+                                FoodItemRow(foodName: brandedItem.foodName, imageURL: brandedItem.photo.thumb)
+                            })
+                            .overlay(
+                                // Add a single long Divider between the columns
+                                Divider().frame(width: 1),
+                                alignment: .leading
+                            )
                             
-                            Image(systemName: "magnifyingglass")
-                                .foregroundStyle(Color.gray)
-                            TextField("Search for food ...", text: $textField)
                         }
-                        .padding(8)
-                    }
-                    
-                    Button(action: {
-                        NutritionixService.shared.searchInstant2(query: textField) { result in
-                            self.foodItems = result
-                        }
-
-                    }, label: {
-                        Text("Search")
-                    })
-                    .padding([.top, .bottom], 10)
-                    
-                    Text("Recent Items")
-                        .font(.caption)
-                        .foregroundStyle(Color.gray)
-                    
-                    if foodItems == nil {
-                        Text("No Recent Items")
-                            .padding(.top, 10)
-                    }
-                    
-                    Spacer()
-                    
-                    if let foodItems = foodItems {
                         
-                        ScrollView {
-                            LazyVGrid(columns:[GridItem(), GridItem()]) {
-                                //                            ForEach(foodItems.common ?? [], id: \.foodName) { commonItem in
-                                //                                FoodItemRow(foodName: commonItem.foodName, imageURL: commonItem.photo.thumb)
-                                //                                    .padding(.bottom,10)
-                                //                            }
-                                
-                                ForEach(foodItems.branded ?? [], id: \.foodName) { brandedItem in
-                                    NavigationLink(destination: FoodDetailView(text: brandedItem.foodName, itemId: brandedItem.nixItemId), label: {
-                                        FoodItemRow(foodName: brandedItem.foodName, imageURL: brandedItem.photo.thumb)
-                                    })
-                                    
-                                    
-                                    
-                                }
-                            }
-                        }
-                    }
+                    }.listRowSeparator(.visible)
+                    
+                    
                     
                 }
-                .padding(.horizontal)
+            }
+            
+        }
+        .padding(.horizontal)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                
                 
             }
         }
+        
     }
 }
 
 #Preview {
-    FoodSearchView()
+    NavigationStack{
+        FoodSearchView()
+    }
 }
