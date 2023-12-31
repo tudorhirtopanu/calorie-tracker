1e64776c72397fb9027c88a52cf90fee6ff58e6b
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-31 21:17:14 +0000

    Add an app delegate to entry point

--- Health App/Health_App.swift
+++ Health App/Health_App.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 import SwiftData
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    let dtm = DailyTaskManager()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+}
+
 @main
 struct Health_App: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             RootView()
---
+++ Health App/Models/DailyTaskManager.swift
@@ -0,0 +1,8 @@
+//
+//  DailyTaskManager.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 31/12/2023.
+//
+
+import Foundation
