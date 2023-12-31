dd56f3800e812ad317e4dfdb2d91b62f0033bbe7
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-31 21:17:38 +0000

    Add class which checks if it is a new day

--- Health App/Models/DailyTaskManager.swift
+++ Health App/Models/DailyTaskManager.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+class DailyTaskManager:ObservableObject {
+    
+    @Published var lastExecutionDate: Date?
+    
+    func tomorrowDate() -> Date {
+            return Calendar.current.date(byAdding: .day, value: 1, to: Date()) ?? Date()
+        }
+    
+//    init(){
+//        scheduleBackgroundTask()
+//    }
+    
+    private func scheduleBackgroundTask() {
+        Task {
+            await performDailyTaskIfNeeded()
+        }
+    }
+    
+     func performDailyTaskIfNeeded() async -> Bool {
+        
+        let currentDate = Calendar.current.startOfDay(for: Date())
+        
+        if isNewDay() {
+            
+            print("Delete Data")
+            UserDefaults.standard.set(currentDate, forKey: "lastDeletionDateTest")
+            return true
+        } else {
+            print("no need to delete data")
+            return false
+        }
+        
+    }
+    
+    func isNewDay() -> Bool {
+        let storedDate = UserDefaults.standard.object(forKey: "lastDeletionDateTest") as? Date
+        let currentDate = Calendar.current.startOfDay(for: Date())
+        
+        if let storedDate = storedDate {
+            return !Calendar.current.isDate(storedDate, inSameDayAs: currentDate)
+        } else {
+            UserDefaults.standard.set(currentDate, forKey: "lastDeletionDateTest")
+            return true
+        }
+        
+    }
+    
+}
