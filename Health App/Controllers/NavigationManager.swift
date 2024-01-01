80a97f09420b6143d9490d993fdbdd5a28cd80c7
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-01 22:48:52 +0000

    Create Navigation Manager

---
+++ Health App/Controllers/NavigationManager.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationManager.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 01/01/2024.
+//
+
+import Foundation
+import SwiftUI
+
+class NavigationManager:ObservableObject {
+    
+    @Published var path:NavigationPath = NavigationPath()
+    
+}
