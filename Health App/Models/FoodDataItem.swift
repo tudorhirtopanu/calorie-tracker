da87d4ca46f4d2fe31dc000d0f1bbb0dd0d59285
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-02 13:47:28 +0000

    update data item to match food struct

--- Health App/Models/FoodDataItem.swift
+++ Health App/Models/FoodDataItem.swift
@@ -14,19 +14,23 @@ class FoodDataItem: Identifiable {
     var id:String
     var name:String
     var foodOccasion:Int
+    var measuredByWeight:Bool
     var calories:Int
     var protein:Double
+    var servingSize:String
     
-    init(name: String, foodOccasion:Int, calories:Int, protein:Double) {
+    init(name: String, foodOccasion:Int, calories:Int, protein:Double, servingSize:String, measuredByWeight:Bool) {
         self.id = UUID().uuidString
         self.name = name
         self.foodOccasion = foodOccasion
         self.calories = calories
         self.protein = protein
+        self.servingSize = servingSize
+        self.measuredByWeight = measuredByWeight
     }
     
     static func example() -> FoodDataItem {
-        return FoodDataItem(name: "Big Mac", foodOccasion: 0, calories: 400, protein: 32)
+        return FoodDataItem(name: "Big Mac", foodOccasion: 0, calories: 400, protein: 32, servingSize: "1 serving", measuredByWeight: true)
     }
     
 }
