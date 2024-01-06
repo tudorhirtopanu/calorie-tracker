6a3264f47ae5fae74a1ce07b36326fac87a53444
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-06 20:08:01 +0000

    add to data

---
+++ Health App/Models/SwiftDataItems/CustomFoodData.swift
@@ -0,0 +1,8 @@
+//
+//  CustomFoodData.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 06/01/2024.
+//
+
+import Foundation
---
+++ Health App/Views/CustomFoodsView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFoodsView.swift
+//  Health App
+//
+//  Created by Tudor Hirtopanu on 06/01/2024.
+//
+
+import SwiftUI
+
+struct CustomFoodsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomFoodsView()
+}
