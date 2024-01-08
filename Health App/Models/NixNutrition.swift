f10f1254bb9c9e92127d961e906b59ccd7a8c49f
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-08 20:09:31 +0000

    uncomment some values which i need

--- Health App/Models/NixNutrition.swift
+++ Health App/Models/NixNutrition.swift
@@ -14,10 +14,10 @@ struct NixNutrition: Decodable {
 struct NixFoods:Decodable {
     let food_name:String
 //    let photo:Photo
-//    let serving_weight_grams:Int
-//    let nf_calories:Int
-//    let nf_protein:Int
-//    let nf_total_carbohydrate:Int
-//    let nf_total_fat:Int
+    let serving_weight_grams:Int
+    let nf_calories:Int
+    let nf_protein:Int
+    //let nf_total_carbohydrate:Int
+    //let nf_total_fat:Int
    
 }
