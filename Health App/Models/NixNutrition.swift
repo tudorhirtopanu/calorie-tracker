d61bf99975acfbddfacd497dfd777749dff594a5
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-08 21:29:44 +0000

    make values optional as api may return null

--- Health App/Models/NixNutrition.swift
+++ Health App/Models/NixNutrition.swift
@@ -14,9 +14,11 @@ struct NixNutrition: Decodable {
 struct NixFoods:Decodable {
     let food_name:String
 //    let photo:Photo
-    let serving_weight_grams:Int
+    let serving_weight_grams:Double?
     let nf_calories:Int
-    let nf_protein:Int
+    let nf_protein:Double?
+    let serving_qty:Double?
+    let serving_unit: String?
     //let nf_total_carbohydrate:Int
     //let nf_total_fat:Int
    
