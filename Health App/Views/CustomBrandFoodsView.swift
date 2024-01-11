4e6c927e40ddd3fe633ce20dc013465b4d3c0fde
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-01-11 15:40:17 +0000

    add search bar to custom brands food view

--- Health App/Views/CustomBrandFoodsView.swift
+++ Health App/Views/CustomBrandFoodsView.swift
@@ -10,13 +10,44 @@ import SwiftUI
 struct CustomBrandFoodsView: View {
         
     @State var selectedFood:[Food] = []
+    @State private var searchText:String = ""
     var fileToDecode:String
     var image:String
     
     @EnvironmentObject var nm:NavigationManager
     
+    private var searchResults:[Food] {
+        
+        if searchText.isEmpty {
+            return selectedFood
+        } else {
+            return selectedFood.filter {
+                            $0.name.range(of: searchText, options: .caseInsensitive) != nil
+                        }
+        }
+        
+    }
+    
     var body: some View {
         VStack {
+            
+            ZStack {
+                
+                RoundedRectangle(cornerSize: CGSize( width: 10, height: 10))
+                    .foregroundStyle(Color.gray.opacity(0.15))
+                    .frame(height: 40)
+                
+                HStack {
+                    
+                    Image(systemName: "magnifyingglass")
+                        .foregroundStyle(Color.gray)
+                    TextField("Search for food ...", text: $searchText)
+                        //.focused($searchIsFocused)
+                }
+                .padding(8)
+            }
+            .padding()
+            
             List {
                 
                 Section {
@@ -34,7 +65,7 @@ struct CustomBrandFoodsView: View {
                     }
                 }
                 
-                ForEach(selectedFood){food in
+                ForEach(searchResults){food in
                     
                     NavigationLink(destination: AddFoodView(foodItem: food, preWrittenFood: true).environmentObject(nm), label: {
                         Text(food.name)
